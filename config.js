b8c9e7389c108d5a67a347831113eefad8e1a06f
SHADOW <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-11-16 17:54:04 +0530

    Fix

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
     VERSION: 'v7.3.6',
     CHANNEL: 'https://t.me/remasterplugin',
-    SESSION: process.env.SHADOW_SESSION === undefined ? '' : process.env.PINKY_SESSION,
+    SESSION: process.env.SHADOW_SESSION === undefined ? '' : process.env.SHADOW_SESSION,
     ANTİLİNK: process.env.ANTİ_LİNK === undefined ? 'false' : process.env.ANTİ_LİNK,
     AUTOBİO: process.env.AUTO_BİO === undefined ? 'false' : process.env.AUTO_BİO,
     AFN: process.env.ALL_CAPTION === undefined ? 'MADE BY PINKY' : process.env.ALL_CAPTION,
