348d537811e8d6944045d2307ad94797efb56aeb
SHADOW <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-11-16 19:20:22 +0530

    Fix

--- pinky.js
+++ pinky.js
@@ -12,6 +12,6 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
- PHONE: process.env.NUMBER === undefined ? '+918301985859' : process.env.NUMBER,   
+ PHONE: process.env.NUMBER === undefined ? '919526808481' : process.env.NUMBER,   
  OA_NAME: process.env.DEPLOYER === undefined ? 'Someone' : process.env.DEPLOYER,    
 };
