75df066ad23fad44c8e3a1aa384c3681ec0ce96a
SHADOW <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-11-17 12:43:39 +0530

    Thumbnail ♥️

--- spark.js
+++ spark.js
@@ -13,6 +13,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
  PHONE: process.env.NUMBER === undefined ? '919526808481' : process.env.NUMBER,   
- OA_NAME: process.env.DEPLOYER === undefined ? 'Someone' : process.env.DEPLOYER,    
+ OA_NAME: process.env.DEPLOYER === undefined ? 'undefined' : process.env.DEPLOYER,    
+ THUMBNAIL: process.env.THUMBNAIL_URL === undefined ? 'https://i.imgur.com/xsKCqwZ.jpeg' : process.env.THUMBNAIL_URL,
  VERIFY: process.env.VERIFICATION_CAPTION === undefined ? '[Shadow Bot] Coded by @SPARK-SHADOW' : process.env.VERIFICATION_CAPTION,
 };
