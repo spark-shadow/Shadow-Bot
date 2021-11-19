5aaa520b6454ecd2788198753aacf09b64a99d1a
Shadow <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-11-19 18:56:15 +0530

    Forward Added♥️

--- plugins/unvoice.js
+++ plugins/unvoice.js
@@ -10,6 +10,7 @@ const ffmpeg = require('fluent-ffmpeg');
 const {execFile} = require('child_process');
 const cwebp = require('cwebp-bin');
 const Config = require('../config');
+const axios = require('axios');
 
 const Language = require('../language');
 const Lang = Language.getString('unvoice'); // Language support
@@ -17,13 +18,14 @@ const Lang = Language.getString('unvoice'); // Language support
 if (Config.STANDPLK == 'off' || Config.STANDPLK == 'OFF') {
 if (Config.WORKTYPE == 'private') {
 
-  MyPnky.addCommand({pattern: 'forward ?(.*)', fromMe: true, desc: Lang.UV_DESC}, (async (message, match) => {    
+  MyPnky.addCommand({pattern: 'send ?(.*)', fromMe: true, desc: 'Forward replied message example: .send jid}, (async (message, match) => {    
     if (message.reply_message === false);
     var location = await message.client.downloadAndSaveMediaMessage({
         key: {
             remoteJid: message.reply_message.jid,
             id: message.reply_message.id
         },
+        var url = await axios.get(Spark.THUMBNAIL, { responseType: 'arraybuffer' })
         message: message.reply_message.data.quotedMessage
     });
 let id = match[1];
@@ -31,7 +33,7 @@ let id = match[1];
         .format('mp3')
         .save('output.mp3')
         .on('end', async () => {
-            await message.client.sendMessage(id, fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true});
+            await message.client.sendMessage(id, fs.readFileSync('output.mp3'), MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true}, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Spark.VERIFY, "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": Buffer.from(url.data)}}}});
 });}));
 
 MyPnky.addCommand({pattern: 'unvoice', fromMe: true, desc: Lang.UV_DESC}, (async (message, match) => {    
