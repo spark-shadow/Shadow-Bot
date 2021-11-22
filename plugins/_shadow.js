0e9785a6e96dd75350fc6c7fd36821cafb76419c
Shadow <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-11-22 10:41:45 +0530

    😁

--- plugins/_shadow.js
+++ plugins/_shadow.js
@@ -103,16 +103,16 @@ var url = await axios.get(Spark.THUMBNAIL, { responseType: 'arraybuffer' })
                 } else {
                     HANDLER = '.';
                 }
-                CMD_HELP += '*🌀 ' + Lang.COMMAND + ':* ```' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
-                if (command.desc !== '') CMD_HELP += '*♦️ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
-                if (command.usage !== '') CMD_HELP += '*📝 ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
-                if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+                CMD_HELP += '🌀 ' + Lang.COMMAND + ':' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '\n\n' : '\n');
+                if (command.desc !== '') CMD_HELP += '♦️ ' + Lang.DESC + ':' + command.desc + (command.warn === '' ? '\n\n' : '\n');
+                if (command.usage !== '') CMD_HELP += '📝 ' + Lang.EXAMPLE + ':' + command.usage + '\n\n';
+                if (command.warn !== '') CMD_HELP += '⚠️ ' + Lang.WARN + ':' + command.warn + '\n\n';
 
             }
         );
         
         await message.client.sendMessage(
-            message.jid, CMD_HELP, MessageType.text , {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Spark.VERIFY, "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": Buffer.from(url.data)}}}}
+            message.jid,'```' + CMD_HELP + '```', MessageType.text , {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Spark.VERIFY, "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": Buffer.from(url.data)}}}}
         );    
     } else {
         var CMD_HELP = '';
@@ -133,17 +133,17 @@ var url = await axios.get(Spark.THUMBNAIL, { responseType: 'arraybuffer' })
                     } else {
                         HANDLER = '.';
                     }
-                    CMD_HELP += '*🌀 ' + Lang.COMMAND + ':* ```' + (cmatch.length >= 3 ? (HANDLER + cmatch[2]) : command.pattern) + (command.desc === '' ? '```\n\n' : '```\n');
-                    if (command.desc !== '') CMD_HELP += '*♦️ ' + Lang.DESC + ':* ```' + command.desc + (command.warn === '' ? '```\n\n' : '```\n');
-                    if (command.usage !== '') CMD_HELP += '*💎 ' + Lang.EXAMPLE + ':* ```' + command.usage + '```\n\n';
-                    if (command.warn !== '') CMD_HELP += '*⚠️ ' + Lang.WARN + ':* ```' + command.warn + '```\n\n';
+                    CMD_HELP += '🌀 ' + Lang.COMMAND + ':' + (cmatch.length >= 3 ? (HANDLER + cmatch[2]) : command.pattern) + (command.desc === '' ? '\n\n' : '\n');
+                    if (command.desc !== '') CMD_HELP += '♦️ ' + Lang.DESC + ':' + command.desc + (command.warn === '' ? '\n\n' : '\n');
+                    if (command.usage !== '') CMD_HELP += '💎 ' + Lang.EXAMPLE + ':' + command.usage + '\n\n';
+                    if (command.warn !== '') CMD_HELP += '⚠️ ' + Lang.WARN + ':' + command.warn + '\n\n';
 
                 }
             }
         );
         if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
         await message.client.sendMessage(
-            message.jid, CMD_HELP, MessageType.text,{quoted: message.data}
+            message.jid,'```' + CMD_HELP + '```', MessageType.text,{quoted: message.data}
         );
     }
 }));
