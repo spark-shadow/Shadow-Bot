2a6073ddb2eda022a440e4054b2e828c741b6339
Shadow <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-11-22 12:58:48 +0530

    Fix

--- plugins/_shadow.js
+++ plugins/_shadow.js
@@ -112,23 +112,40 @@ var url = await axios.get(Spark.THUMBNAIL, { responseType: 'arraybuffer' })
         );
         
         await message.client.sendMessage(
-            message.jid,'```\n' + Config.BOTPLK + '\n\n❐ Command List\n╭────────────────────────────╮\n' + CMD_HELP + '╰────────────────────────────╯\n' + '```', MessageType.text , {quoted : {
-  key: {
-    fromMe: false,
-    participant: "0@s.whatsapp.net",
-    remoteJid: "status@broadcast"
-  },
-  message: {
-        "orderMessage": {
-        	"itemCount" : 2021,
-             "status": 1,
-           "surface" : 1,
-           "message": Config.VERIFY,
-           "orderTitle": "menu",
-           "sellerJid": '919526808481@s.whatsapp.net' 
-        }
-      }
-});
+            message.jid,'```\n' + Config.BOTPLK + '\n\n❐ Command List\n╭────────────────────────────╮\n' + CMD_HELP + '╰────────────────────────────╯\n' + '```', MessageType.text , {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Spark.VERIFY, "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": Buffer.from(url.data)}}}}
+        );    
+    } else {
+        var CMD_HELP = '';
+        Shadow.commands.map(
+            async (command) =>  {
+                if (command.dontAddCommandList || command.pattern === undefined) return;
+                try {
+                    var cmatch = command.pattern.toString().match(/(\W*)([A-Za-zğüşiöç1234567890]*)/);
+                } catch {
+                    var cmatch = [command.pattern];
+                }
+                
+                if (cmatch[2] == match[1]) {
+                    var HANDLER = '';
+    
+                    if (/\[(\W*)\]/.test(Config.HANDLERS)) {
+                        HANDLER = Config.HANDLERS.match(/\[(\W*)\]/)[1][0];
+                    } else {
+                        HANDLER = '.';
+                    }
+                    CMD_HELP += '│❐ ' + (match.length >= 3 ? (HANDLER + match[2]) : command.pattern) + (command.desc === '' ? '\n\n' : '\n');
+                if (command.desc !== '') CMD_HELP += '│➣ ' + command.desc + (command.warn === '' ? '\n\n' : '\n');
+                if (command.usage !== '') CMD_HELP += '│➩ ' + command.usage + '\n\n';
+                if (command.warn !== '') CMD_HELP += '│☞ ' + command.warn + '\n\n';
+
+                }
+            }
+        );
+        if (CMD_HELP === '') CMD_HELP += Lang.NOT_FOUND;
+        await message.client.sendMessage(
+            message.jid,'```\n' + Config.BOTPLK + '\n\n❐ Command List\n╭────────────────────────────╮\n' + CMD_HELP + '╰────────────────────────────╯\n' + '```', MessageType.text,{quoted: message.data}
+        );
+    }
 }));
 }
 }
