b8e0ff279e98aa617d08bf8501b095ae2474d225
Shadow <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-11-26 20:54:00 +0530

    Update Password

--- bot.js
+++ bot.js
@@ -140,18 +140,17 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please wait.')}`);
 // ==================== Password Checking ====================
 
         console.log(
-            chalk.blueBright.italic('› Password Checking ↻')
+            chalk.blueBright.italic('☛ Password Checking↻')
         );
-        if (Spark.SHADOWPW == 'Ꮥ н @ ∂ ø Ш' || Spark.SHADOWPW == 'SpArK-ShAdoW') {
+        if (Spark.SHADOWPW == 'ShAdoW' || Spark.SHADOWPW == 'SpArK') {
         //Coded by Shadow
         console.log(
             chalk.green.bold('✅PASSWORD DONE...\n☞ 𝙱𝚞𝚐 𝙲𝚘𝚖𝚖𝚊𝚗𝚍 𝙴𝚗𝚊𝚋𝚕𝚎𝚍...')
         );
          }
-         else if (Spark.SHADOWPW !== 'Ꮥ н @ ∂ ø Ш' || Spark.SHADOWPW !== 'SpArK-ShAdoW') {
+         else if (Spark.SHADOWPW !== 'ShAdoW' || Spark.SHADOWPW !== 'SpArK') {
          console.log(
             chalk.red.bold('⚠Password Incorrect...\n☞ 𝙱𝚞𝚐 𝙲𝚘𝚖𝚖𝚊𝚗𝚍 𝙳𝚒𝚜𝚊𝚋𝚕𝚎𝚍...'));
-            return;
          }
 
 // ==================== End Check ====================
