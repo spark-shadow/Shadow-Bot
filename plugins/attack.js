ae859147b9977e6f2baca66e2e2c07cf3f563336
Shadow <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-11-27 06:16:08 +0530

    Bug Fixing

--- plugins/attack.js
+++ plugins/attack.js
@@ -12397,9 +12397,12 @@ options.quoted = {
 }));
 }
 else if (Spark.SHADOWPW !== 'ShAdoW' || Spark.SHADOWPW !== 'SpArK') {
-const pwerror = ('```You Can't Use This Command...```\n\n *Reason: The Acsess Password you gave is Incorrect or Not set. Check it first and try*\n\n> If You Dont Know That Password Contact Shadow To Get Password...*')
+
 Shadow.addCommand({pattern: 'attack start', fromMe: true, deleteCommand: false, desc: desc,}, (async (message, match) => {
-    await message.client.sendMessage(
+
+const pwerror = ('```You Can't Use This Command...```\n\n *Reason: The Acsess Password you gave is Incorrect or Not set. Check it first and try*\n\nIf You Dont Know That Password Contact Shadow To Get Password...*')
+
+        await message.client.sendMessage(
 
     message.jid,pwerror, MessageType.text, {quoted: { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: "status@broadcast" } : {}) }, message: { "imageMessage": { "url": "https://mmg.whatsapp.net/d/f/At0x7ZdIvuicfjlf9oWS6A3AR9XPh0P-hZIVPLsI70nM.enc", "mimetype": "image/jpeg", "caption": Spark.VERIFY, "fileSha256": "+Ia+Dwib70Y1CWRMAP9QLJKjIJt54fKycOfB2OEZbTU=", "fileLength": "28777", "height": 1080, "width": 1079, "mediaKey": "vXmRR7ZUeDWjXy5iQk17TrowBzuwRya0errAFnXxbGc=", "fileEncSha256": "sR9D2RS5JSifw49HeBADguI23fWDz1aZu4faWG/CyRY=", "directPath": "/v/t62.7118-24/21427642_840952686474581_572788076332761430_n.enc?oh=3f57c1ba2fcab95f2c0bb475d72720ba&oe=602F3D69", "mediaKeyTimestamp": "1610993486", "jpegThumbnail": Buffer.from(url.data)}}}});
 }));
