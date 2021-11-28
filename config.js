4d72be7bc9a18eee3f3a1ddb96a65903791ba6ee
Shadow <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-11-28 11:19:21 +0530

    Bgm Duration

--- config.js
+++ config.js
@@ -21,6 +21,7 @@ module.exports = {
     SESSION: process.env._SHADOW_SESSION === undefined ? '' : process.env._SHADOW_SESSION,
     RBG_API_KEY: process.env.RBG_API_KEY === undefined ? '' : process.env.RBG_API_KEY,
     SHADOWPW: process.env._SHADOW_BOT_ACSESS_PIN_PASSWORD === undefined ? false : process.env._SHADOW_BOT_ACSESS_PASSWORD,
+    DURATION: process.env.BGM_DURATION === undefined ? '0' : process.env.BGM_DURATION,
     BOT_PRESENCE: process.env.BOT_PRESENCE === undefined ? 'online' : process.env.BOT_PRESENCE,
     ANTİLİNK: process.env.ANTİ_LİNK === undefined ? 'false' : process.env.ANTİ_LİNK,
     AUTOBİO: process.env.AUTO_BİO === undefined ? 'false' : process.env.AUTO_BİO,
