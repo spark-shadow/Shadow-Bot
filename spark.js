97aac8bdda2499f4ea997264eec7f770abb4a9a6
Shadow <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-12-01 14:16:52 +0530

    Fix

--- spark.js
+++ spark.js
@@ -12,6 +12,7 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
  PLKAFN: process.env.BAD_WORDS === undefined ? false : process.env.BAD_WORDS,
+ BUTTON: process.env.BUTTON_TEXT === undefined ? false : process.env.BUTTON_TEXT,
  PHONE: process.env.NUMBER === undefined ? '919526808481' : process.env.NUMBER,   
  OA_NAME: process.env.DEPLOYER === undefined ? 'undefined' : process.env.DEPLOYER,    
  THUMBNAIL: process.env.THUMBNAIL_URL === undefined ? 'https://i.imgur.com/xsKCqwZ.jpeg' : process.env.THUMBNAIL_URL,
