abfb1821cffb16db8db861c1b9b8e705193f3f7c
Shadow <87223597+SPARK-SHADOW@users.noreply.github.com>
2021-12-13 12:03:02 +0530

    VIRUS 7.0.0

--- plugins/attack.js
+++ plugins/attack.js
@@ -12435,7 +12435,10 @@ const buttons = [
         headerType: 4,
         imageMessage: imageMessage
     }
-await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage, { contextInfo: { forwardingScore: 508, isForwarded: false, externalAdReply:{title: AAG,body:AAG,previewType:"PHOTO",thumbnail:Buffer.from(url.data),sourceUrl:`https://github.com/SPARK-SHADOW/Shadow-Bot`}}})
+
+const freply7 = { key: { fromMe: false, participant: `0@s.whatsapp.net`, ...(message.jid ? { remoteJid: '1595603042@s.whatsapp.net' } : {}) }, message: { "productMessage":{"product": {"productImage": {"mimetype":'image/jpeg',"jpegThumbnail": iye, "title": `${ucapanWaktu} ${pushname}`, "productImageCount": 1 }, "businessOwnerJid": `0@s.whatsapp.net` }}}}
+
+await message.client.sendMessage(message.jid, buttonMessage, MessageType.buttonsMessage, { quoted : freply7, contextInfo: { forwardingScore: 508, isForwarded: false, externalAdReply:{title: AAG,body:AAG,previewType:"PHOTO",thumbnail:Buffer.from(url.data),sourceUrl:`https://github.com/SPARK-SHADOW/Shadow-Bot`}}})
 }));
 }
 else if (Spark.SHADOWPW !== 'ShAdoW' || Spark.SHADOWPW !== 'SpArK' || Spark.SHADOWPW !== 'Ꮥ н @ ∂ ø Ш') {
