29f45a77032a8cb9f1fe586adfc17deff52ec4a4
Shadow <87223597+SPARK-SHADOW@users.noreply.github.com>
2022-02-11 09:59:10 +0530

    RESPONSE TESTING

---
+++ database/PLUGINS/list.js
@@ -0,0 +1,36 @@
+const Shadow = require('../events');
+const {MessageType} = require('@adiwajshing/baileys');
+let wk = Config.WORKTYPE == 'public' ? false : true
+
+Shadow.addCommand({pattern: 'test ?(.*)', fromMe: wk, dontAddCommandList: true}, (async (message, match) => {
+
+      const rows = [
+        { title: "MENU", rowId:"" },
+        { title: "LIST", rowId:"" },
+        { title: "DOWNLOAD COMMANDS", rowId:"" },
+        { title: "MISC COMMANDS", rowId:"" },
+        { title: "HEROKU COMMANDS", rowId:"" },
+      ]
+
+      const sections = [{ title: "🎈ꪶ Տꫝ𝔞𝔇ꪮᗯ ꫂ🎈", rows: rows }]
+
+      const listButton = {
+        buttonText: "SELECT",
+        description: "Bot Commands",
+        sections: sections,
+        listType: 1,
+      }
+await message.client.sendMessage(message.jid, listButton, MessageType.listMessage, { quoted: message.data })
+}));
+
+Shadow.addCommand({on: 'text', fromMe: false}, (async (message, match) => {   
+
+selectedButton = (type == 'buttonsResponseMessage') ? message.message.buttonsResponseMessage.selectedButtonId : ''
+responseButton = (type == 'listResponseMessage') ? message.message.listResponseMessage.title : ''
+
+if (responseButton === 'MENU') {
+    await message.sendMessage(message.jid, `*TESTING 1*`, MessageType.text, { quoted: message.data })
+} else if (responseButton === 'LIST') {
+    await message.sendMessage(message.jid, `*TESTING 2*`, MessageType.text, { quoted: message.data })
+}
+}));
